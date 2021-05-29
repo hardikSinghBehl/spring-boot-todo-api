d758594358908dcd3f954f7249c9930bbd75711c
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-29 14:40:21 +0530

    initial dummy project commit

---
+++ src/main/java/com/hardik/donatello/SpringReactTodoApiApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.donatello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactTodoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactTodoApiApplication.class, args);
+	}
+
+}
