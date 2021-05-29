4d9af7d7e3dafb6fa9788c71afa2172b74c902ee
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-29 16:28:27 +0530

    updating property name for Open-API configuration

--- src/main/java/com/hardik/donatello/configuration/properties/OpenApiConfigurationProperties.java
+++ src/main/java/com/hardik/donatello/configuration/properties/OpenApiConfigurationProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import lombok.Data;
 
 @Data
-@ConfigurationProperties(prefix = "com.hardik.plutocracy.swagger")
+@ConfigurationProperties(prefix = "com.hardik.donatello.swagger")
 public class OpenApiConfigurationProperties {
 
 	private Properties properties = new Properties();
