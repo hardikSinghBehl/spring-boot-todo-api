241d44239074f8a59298e1d7098d6afb002be466
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-29 14:44:26 +0530

    added entity classes

---
+++ src/main/java/com/hardik/donatello/entity/Todo.java
@@ -0,0 +1,75 @@
+package com.hardik.donatello.entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.Data;
+import lombok.EqualsAndHashCode.Exclude;
+
+@Entity
+@Table(name = "todos")
+@Data
+public class Todo implements Serializable {
+
+	private static final long serialVersionUID = -2583760636558947711L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", nullable = false, unique = true)
+	private UUID id;
+
+	@Hidden
+	@Exclude
+	@ManyToOne(optional = false, fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false, insertable = false, updatable = false)
+	private User user;
+
+	@Column(name = "user_id", nullable = false)
+	private UUID userId;
+
+	@Column(name = "title", nullable = false)
+	private String title;
+
+	@Column(name = "description", nullable = false)
+	private String description;
+
+	@Column(name = "due_date", nullable = false)
+	private LocalDate dueDate;
+
+	@Column(name = "is_active", nullable = false)
+	private boolean isActive;
+
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@Column(name = "updated_at", nullable = false)
+	private LocalDateTime updatedAt;
+
+	@PrePersist
+	public void onCreate() {
+		this.id = UUID.randomUUID();
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+	}
+
+	@PreUpdate
+	public void onUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+
+}
